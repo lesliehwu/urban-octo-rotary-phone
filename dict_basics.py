a41e63964ec21c07008ae063102d69af59fe5e68
lesliehwu <mslesliewu@gmail.com>
2017-09-05 19:49:46 -0700

    first commit

---
+++ dict_basics.py
@@ -0,0 +1,7 @@
+my_dick = {"name":"Sadie","age":"3","country of birth":"San Ramon","favorite language":"bork"}
+
+def dict_basics(a_dick):
+    for alicia_keys,data in a_dick.items():
+        print("My",alicia_keys,"is",data)
+
+print(dict_basics(my_dick))
